b6d149b28ec8b6ee9aa7dbfdd27727b28df83078
lenny_cheng <langrisser1981@gmail.com>
2022-12-23 01:59:21 +0800

    可作動

--- Combine-Demo-github-browse/ContentView.swift
+++ Combine-Demo-github-browse/ContentView.swift
@@ -5,17 +5,77 @@
 //  Created by 程信傑 on 2022/12/22.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var searchModel = SearchViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List($searchModel.repos) { $repo in
+                let name = repo.name
+                Text(name)
+            }
+            .searchable(text: $searchModel.searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+            .navigationTitle("Search")
+//                .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+    }
+}
+
+class SearchViewModel: ObservableObject {
+    @Published var repos: [GitRepo] = []
+    @Published var searchText: String = ""
+    private var cancellableSet = Set<AnyCancellable>()
+
+    init() {
+        $searchText
+            .throttle(for: 0.5, scheduler: RunLoop.main, latest: false)
+            .removeDuplicates()
+            .flatMap { keyword in
+                let baseURL = "https://api.github.com"
+                let path = "/users/\(keyword)/repos"
+                let url = URL(string: baseURL + path)
+                let request = URLSession.shared.dataTaskPublisher(for: url!)
+                return request
+                    .tryMap { data, response in
+                        guard let httpResponse = response as? HTTPURLResponse,
+                              httpResponse.statusCode == 200 else { throw NetworkError.badRequest }
+//                        print(String(decoding: data, as: UTF8.self))
+                        return data
+                    }
+                    .decode(type: [GitRepo].self, decoder: JSONDecoder())
+                    .print()
+                    .catch { _ in
+                        Empty<[GitRepo], Never>()
+                    }
+            }
+            .receive(on: RunLoop.main)
+            .assign(to: &$repos)
+    }
+}
+
+enum NetworkError: Error {
+    case badRequest
+}
+
+struct Owner: Codable {
+    var name: String
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+    }
+}
+
+struct GitRepo: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var owner: Owner
+    var isPrivate: Bool
+    var url: String
+    enum CodingKeys: String, CodingKey {
+        case id, name, owner, url
+        case isPrivate = "private"
     }
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+#  Combine_Demo_github_repo_browse
