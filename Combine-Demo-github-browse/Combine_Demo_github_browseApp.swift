9557f331a2b01bf20e79f14536d255308e10e877
lenny_cheng <langrisser1981@gmail.com>
2022-12-22 22:24:22 +0800

    Initial Commit

---
+++ Combine-Demo-github-browse/Combine_Demo_github_browseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_Demo_github_browseApp.swift
+//  Combine-Demo-github-browse
+//
+//  Created by 程信傑 on 2022/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Combine_Demo_github_browseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
